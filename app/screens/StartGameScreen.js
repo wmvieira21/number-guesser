503f2ad55a2a91f3b821703e6a5ce2c1f6bb5ad1
wmvieira21 <williammartinsvieira334@gmail.com>
2025-09-22 12:23:21 -0300

    first commit

---
+++ app/screens/StartGameScreen.js
@@ -0,0 +1,61 @@
+import { StyleSheet, TextInput, View } from "react-native";
+import PrimaryButton from "../../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        maxLength={2}
+        keyboardType="numeric"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.inputContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onClick={() => {}}>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onClick={() => {}}>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#4d082aff",
+    alignItems: "center",
+    padding: 16,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    marginTop: 100,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  inputContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 16,
+  },
+  textInput: {
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    width: 50,
+    textAlign: "center",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
+
+export default StartGameScreen;
---
+++ components/PrimaryButton.js
@@ -0,0 +1,33 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+export default function PrimaryButton({ onClick, children }) {
+  return (
+    <Pressable
+      onPress={onClick}
+      android_ripple={{ color: "#33001aff" }}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
+      <View style={styles.container}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#77073fff",
+    borderRadius: 28,
+    paddingVertical: 12,
+    paddingHorizontal: 18,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
